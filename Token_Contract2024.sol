264b8f86c9a8eb8290cb3717b7ce3dfc9b7b280e
Paul <84129217+DRIVENpol@users.noreply.github.com>
2024-05-01 02:37:35 +0300

    Update Token_Contract2024.sol

--- Token_Contract2024.sol
+++ Token_Contract2024.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BlueSocialToken is ERC20, Ownable {
+    constructor(address initialOwner)
+        ERC20("Blue Social Token", "BLUE")
+        Ownable(initialOwner)
+    {
+        _mint(initialOwner, 2_500_000_000 * 10 ** decimals());
+    }
+}
